73ba128635a43d5705cf0c86c8e9605141f0d40f
sampath-houde <houdesampath190701@gmail.com>
2020-11-11 17:21:25 +0530

    Added View Model to Login, Register, ForgotPassword1 and ForgotPassword2 Fragment

---
+++ app/src/main/java/com/example/Movies/login_register/views/LoginRegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.Movies.login_register.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.ActivityNavigator
+import com.example.Movies.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginregister)
+    }
+}
